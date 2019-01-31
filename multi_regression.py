e2a280f0b883296b56fba51529d95f85dc4fedc7
Fatemeh Kiaie <40069260+fatemehKi@users.noreply.github.com>
2019-01-31 11:47:22 -0500

    Update multi_regression.py

--- multi_regression.py
+++ multi_regression.py
@@ -26,7 +26,7 @@ onehotencoder = OneHotEncoder(categorical_features = [3])
 X = onehotencoder.fit_transform(X).toarray()
 
 # to avoid the dummy variable trap, we need to remove the first 
-
+X = X[:, 1:]
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
@@ -39,3 +39,14 @@ X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
 
+
+#Fitting multiple linear regression on the training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+
+#Backward Elimination for building the optimal model
+import statsmodels.formula.api as sm
+X = np.append(arr = np.ones((50, 1)).astype(int), values=X, axis= 1 )
