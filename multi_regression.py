093ff966ca3033d79c8b87811d811c145c37c13c
Fatemeh Kiaie <40069260+fatemehKi@users.noreply.github.com>
2019-01-31 10:51:45 -0500

    Update multi_regression.py

--- multi_regression.py
+++ multi_regression.py
@@ -24,3 +24,17 @@ labelencoder_X = LabelEncoder()
 X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
 onehotencoder = OneHotEncoder(categorical_features = [3])
 X = onehotencoder.fit_transform(X).toarray()
+
+# to avoid the dummy variable trap, we need to remove the first 
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
